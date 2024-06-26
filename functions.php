8cf8b46bd24b7b894b293ac4c3a41e2c46bb9637
Nikos Roussos <nikos.roussos@greenpeace.org>
2024-06-26 10:44:56 +0300

    Add temp prod domain to CORS

--- functions.php
+++ functions.php
@@ -1,13 +1,20 @@
 <?php
 
 /**
- * Additional code for the child theme goes in here.
- */
+* Additional code for the child theme goes in here.
+*/
 
 add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
 
 function enqueue_child_styles() {
-	$css_creation = filectime(get_stylesheet_directory() . '/style.css');
+  $css_creation = filectime(get_stylesheet_directory() . '/style.css');
 
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
+
+add_filter(
+  'planet4_csp_allowed_frame_ancestors',
+  function($allowlist) {
+    return array_merge($allowlist, ['www-prod.greenpeace.org']);
+  }
+);
